e666de0238b8956ae892f528a4c8f080659ffaa5
Gabriel Lundgren <glundgren93@gmail.com>
2021-12-07 16:46:25 +0000

    Update dbConnector.js

--- server/src/dbconfig/dbConnector.js
+++ server/src/dbconfig/dbConnector.js
@@ -4,7 +4,7 @@ const pool = new Pool({
   max: 20,
   //connectionString: 'postgres://root:newPassword@localhost:port/dbname',
   // connectionString: "postgres://user:password@db:5432/todo-db",
-  connectionString: "postgres://user:password@localhost:5432/todo-db",
+  connectionString: "postgres://user:password@db:5432/todo-db",
   idleTimeoutMillis: 30000,
 });
 
